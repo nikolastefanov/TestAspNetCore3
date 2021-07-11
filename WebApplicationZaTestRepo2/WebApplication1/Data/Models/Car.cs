825971f34706bc884802109afc4fb292fcf8ab37
nikolastefanov <nikolay_stefanov1965@abv.bg>
2021-07-11 11:26:26 +0300

    initialize

---
+++ WebApplicationZaTestRepo2/WebApplication1/Data/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+    }
+}
